e13121fbd19006f52c57fa97115afa49cfb97d24
Alan Fitzpatrick <alajfit@gmail.com>
2015-10-13 11:44:11 +0100

    Wrapping up controllers

--- basics/5_scope/index.php
+++ basics/5_scope/index.php
@@ -2,8 +2,12 @@
   /***
    * Scope levels - Parent and child
    * ---------------------------------------
-   *
-   *
+   * Here we can see the scope of the parent is also the scope of the child
+   * This is due to prototypical behaviour
+   * Child inherits all the attributes of the parent here
+   * The child checks if it has defined a variable if not it will inherit from the parent
+   *  - when this happens all changes are passed to the parent
+   * Controllers are nested just like DOM elements, Important to note this 
    ***/
 ?>
 
